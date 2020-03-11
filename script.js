242278c10fc549beb53a37ea5f361f06822806d4
Matteo <matteo.inchi97@gmail.com>
2020-03-11 09:56:26 +0100

    indexUI.html

---
+++ script.js
@@ -0,0 +1,71 @@
+let div = document.querySelector('div');
+
+let div2 = document.createElement('div');
+div.appendChild(div2);
+
+let div3 = document.createElement('div');
+div.appendChild(div3);
+
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    div2.textContent = playRound(button.className, computerPlay());
+    if(computerPoints === 5) {
+      div3.textContent = "Computer vince!";
+    }else if(playerPoints === 5){
+      div3.textContent = "Giocatore vince!"
+    }else{
+      div3.textContent = "In attesa dei risultati.. "
+    }
+  });
+});
+
+
+let computerPoints = 0;
+let playerPoints = 0;
+
+let element = ["rock", "paper", "scissors"];
+
+//questa funzione prende in maniera casuale uno degli elementi del vettore
+function computerPlay() {
+  return element[Math.floor(Math.random() * element.length)];
+}
+
+let playerSelection;
+let computerSelection;
+
+//funzione del singolo round
+function playRound(playerSelection, computerSelection) {
+
+playerSelection = playerSelection.replace(/rock/i, 'rock');
+playerSelection = playerSelection.replace(/paper/i, 'paper');
+playerSelection = playerSelection.replace(/scissors/i, 'scissors');
+
+
+  if(playerSelection === 'rock' && computerSelection === 'paper') {
+    return ('Computer vince, il risultato è :' + playerPoints + "-" + ++computerPoints);
+  } else if(playerSelection === 'rock' && computerSelection === 'scissors') {
+    return ('Giocatore vince, il risultato è :' + ++playerPoints + "-" + computerPoints);
+  } else if(playerSelection === 'paper' && computerSelection === 'rock') {
+    return ('Giocatore vince, il risultato è :' + ++playerPoints + "-" + computerPoints);
+  } else if(playerSelection === 'paper' && computerSelection === 'scissors') {
+    return ('Computer vince, il risultato è :' + playerPoints + "-" + ++computerPoints);
+  } else if(playerSelection === 'scissors' && computerSelection === 'paper') {
+    return ('Giocatore vince, il risultato è :' + ++playerPoints + "-" + computerPoints);    
+  } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    return ('Computer vince, il risultato è :' + playerPoints + "-" + ++computerPoints);
+  } else if (playerSelection == computerSelection) {
+    return ('Pareggio');
+  } else {
+    return ('Errore');
+  }
+}
+
+
+
+  
+
+
+
+
